7dc5f83803e07dadae6f36ec367a925617e9d8f5
PengfeiHePower <billhe@yahoo.com>
2023-09-03 09:53:16 -0400

    modify

--- hidden-repre/synthesis_train.py
+++ hidden-repre/synthesis_train.py
@@ -17,19 +17,19 @@ from torchvision.datasets import CIFAR10
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-parser = argparse.ArgumentParser(description='hidden-rep-back-test')
-parser.add_argument('--datapath', default='synthesis/cifar10/fsgm_', type=str)
+parser = argparse.ArgumentParser(description='syhthesis-train')
+parser.add_argument('--datapath', default='synthesis/cifar10/adversarial_data/resnet18/fsgm_train_all50000', type=str)
 parser.add_argument('--lr_max', default=0.1, type=float, help='learning rate')
 parser.add_argument('--batch', default=128, type=int, help='batch size')
 parser.add_argument('--epochs', default = 200, type=int, help='training epochs')
-parser.add_argument('--modelsaver', default = 'pretrained/fsgm_rn18.pth')
+parser.add_argument('--modelsaver', default = 'pretrained/fsgm_syn_rn18.pth')
 args = parser.parse_args()
 print(args)
 
 class PoisonTransferCIFAR10Pair(CIFAR10):
     """CIFAR10 Dataset.
     """
-    def __init__(self, datapath, root='~/Documents/cse-resarch/data', train=True, transform=None, download=True):
+    def __init__(self, datapath, root='~/Documents/cse-resarch/data/cifar10', train=True, transform=None, download=True):
         super(PoisonTransferCIFAR10Pair, self).__init__(root=root, train=train, download=download, transform=transform)
         self.datapath = datapath
         self.data = (np.load(self.datapath + '_img.npy').transpose([0, 2, 3, 1]) * 255).astype(np.uint8)
@@ -55,8 +55,6 @@ def train(epoch, net, optimizer, trainloader):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        # lr = lr_sch(epoch + (batch_idx + 1) / len(trainloader))
-        # optimizer.param_groups[0].update(lr=lr)
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -91,13 +89,6 @@ def test(epoch, net, testloader):
     print('Test Loss:', test_loss/(batch_idx+1))
     print('Test Acc:', 100.*correct/total)
 
-    # Save checkpoint.
-    # acc = 100.*correct/total
-    # acc_test.append(acc)
-    # loss_avg = test_loss/len(trainloader)
-    # loss_test.append(loss_avg)
-    # if acc > best_acc:
-    #     print('Saving..')
     state = {
         'net': net.state_dict(),
         'epoch': epoch,
@@ -115,21 +106,18 @@ transform_test = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-trainpath = args.datapath+'train'
-trainset = PoisonTransferCIFAR10Pair(datapath = trainpath, train=True, transform=transform_train, download=False)
+trainset = PoisonTransferCIFAR10Pair(datapath = args.datapath, train=True, transform=transform_train, download=False)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch, shuffle=True, num_workers=4)
 
-testpath = args.datapath+'test'
-testset = PoisonTransferCIFAR10Pair(datapath = testpath, train=False, transform=transform_train, download=False)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True, num_workers=4)
-
+testset = torchvision.datasets.CIFAR10(root='~/Documents/cse-resarch/data/cifar10', train=False, download=False, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
 # prepare model
 print('==> Building model..')
 net = ResNet18()
 net = net.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr_max, momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[80, 120], gamma=0.1)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
 
 # training
 for epoch in range(args.epochs): # 200 epochs
